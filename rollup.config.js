e07e729e2685835537f20d6069dd55197bc3e471
Andreas Stassivik <andreas@stassi.net>
2019-01-29 03:26:35 -0800

    bundler config

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+// rollup-plugin-terser does not support multiple outputs
+const options = {
+  input: 'src/index.js',
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ exclude: 'node_modules/**' }),
+    terser(),
+  ],
+};
+
+export default [
+  {
+    ...options,
+    output: {
+      file: 'lib/implausible.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  },
+  {
+    ...options,
+    output: {
+      file: 'lib/implausible.umd.js',
+      format: 'umd',
+      name: 'spaceships',
+      sourcemap: true,
+    },
+  }
+];
