aa133a6e91267c3fb2007566df31a40251791c84
Andreas Stassivik <andreas@stassi.net>
2019-01-29 20:33:19 -0800

    prngs merged into prng

--- src/prng.js
+++ src/prng.js
@@ -1,12 +1,14 @@
-import {
-  alea,
-  arc4,
-  tychei,
-  xor128,
-  xor4096,
-  xorshift7,
-  xorwow,
-} from './prngs';
+import seedrandom from 'seedrandom';
+
+const arc4 = seed => seedrandom(seed)();
+const algorithm = name => seed => seedrandom[name](seed)();
+
+const alea = algorithm('alea');
+const tychei = algorithm('tychei');
+const xor128 = algorithm('xor128');
+const xor4096 = algorithm('xor4096');
+const xorshift7 = algorithm('xorshift7');
+const xorwow = algorithm('xorwow');
 
 const selectPrng = ({
   seed,
