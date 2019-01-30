0f65f5f09087344ce5a0f3b433251b39fb7e516d
Andreas Stassivik <andreas@stassi.net>
2019-01-29 20:55:40 -0800

    simplification

--- src/prng.js
+++ src/prng.js
@@ -1,28 +1,14 @@
 import seedrandom from 'seedrandom';
 
-const arc4 = seed => seedrandom(seed)();
-const algorithm = name => seed => seedrandom[name](seed)();
-
-const alea = algorithm('alea');
-const tychei = algorithm('tychei');
-const xor128 = algorithm('xor128');
-const xor4096 = algorithm('xor4096');
-const xorshift7 = algorithm('xorshift7');
-const xorwow = algorithm('xorwow');
+const arc4 = seed => seedrandom(seed);
 
 const selectPrng = ({
   seed,
   name = 'arc4',
-}) => ({
-  alea,
-  arc4,
-  tychei,
-  xor128,
-  xor4096,
-  xorshift7,
-  xorwow,
-}[name])(seed || arc4());
+}) => name === 'arc4'
+  ? arc4(seed)
+  : seedrandom[name](seed || arc4());
 
-const prng = (params = {}) => selectPrng(params);
+const prng = (params = {}) => selectPrng(params)();
 
 export default prng;
