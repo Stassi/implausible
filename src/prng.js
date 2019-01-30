9044864b832c16e96fd1c91e398f5f84cfb7b542
Andreas Stassivik <andreas@stassi.net>
2019-01-29 21:04:27 -0800

    seed application extracted

--- src/prng.js
+++ src/prng.js
@@ -2,13 +2,15 @@ import seedrandom from 'seedrandom';
 
 const arc4 = seed => seedrandom(seed);
 
-const selectPrng = ({
+const choosePrng = name => name === 'arc4'
+  ? arc4
+  : seedrandom[name];
+
+const applySeed = ({
   seed,
   name = 'arc4',
-}) => name === 'arc4'
-  ? arc4(seed)
-  : seedrandom[name](seed || arc4());
+}) => choosePrng(name)(seed || arc4());
 
-const prng = (params = {}) => selectPrng(params)();
+const prng = (params = {}) => applySeed(params)();
 
 export default prng;
