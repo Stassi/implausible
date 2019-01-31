e7ae34df68eb25306da5882a1f04508597efa8fb
Andreas Stassivik <andreas@stassi.net>
2019-01-31 01:54:49 -0800

    +(generated, namesByDescendingWeight, totalWeight)

--- src/weighted.js
+++ src/weighted.js
@@ -1,4 +1,74 @@
+import {
+  descend,
+  map,
+  pipe,
+  prop,
+  sort,
+  sum,
+  toPairs,
+  values,
+} from 'ramda';
+import prng from './prng';
+
+const toGenerated = ({ seed, ...props }) => ({
+  ...props,
+  generated: prng({ seed }),
+});
+
+const sumOfValues = pipe(
+  values,
+  sum,
+);
+
+const toTotalWeight = ({ distribution, ...props }) => ({
+  ...props,
+  distribution,
+  totalWeight: sumOfValues(distribution),
+});
+
+const namesByDescendingWeight = pipe(
+  toPairs,
+  map(([name, weight]) => ({ name, weight })),
+  sort(
+    descend(
+      prop('weight'),
+    ),
+  ),
+);
+
+const toNamesByDescendingWeight = ({
+  distribution,
+  ...props,
+}) => ({
+  ...props,
+  namesByDescendingWeight: namesByDescendingWeight(distribution),
+});
+
 // TODO: Implement
-const weighted = () => true;
+const debug = ({
+  namesByDescendingWeight,
+  totalWeight,
+  ...props
+}) => {
+  // TODO: Implement reducer
+  const debugTwo = [
+    namesByDescendingWeight[0].weight / totalWeight,
+    namesByDescendingWeight[0].weight / totalWeight
+      + namesByDescendingWeight[1].weight / totalWeight,
+    namesByDescendingWeight[0].weight / totalWeight
+      + namesByDescendingWeight[1].weight / totalWeight
+      + namesByDescendingWeight[2].weight / totalWeight,
+  ];
+  const res = { ...props, debugTwo };
+  return res;
+};
+
+// TODO: Implement
+const weighted = pipe(
+  toGenerated,
+  toTotalWeight,
+  toNamesByDescendingWeight,
+  debug,
+);
 
 export default weighted;
--- test/weighted.js
+++ test/weighted.js
@@ -2,7 +2,15 @@ import { expect } from 'chai';
 import { weighted } from '../src';
 
 describe('weighted pseudorandom number generator', () => {
+  // TODO: Rename
   it('should have tests', () => {
-    expect(weighted()).to.be.false;
+    expect(weighted({
+      distribution: {
+        x: 4,
+        y: 1,
+        z: 2,
+      },
+      seed: 'hello.',
+    })).to.equal('y');
   });
 });
