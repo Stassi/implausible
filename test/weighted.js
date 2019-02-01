676437fabf5452dfae2a4936ab33abe28d1b361c
Andreas Stassivik <andreas@stassi.net>
2019-01-31 21:23:57 -0800

    +(stochastic, deterministic) tests

--- test/weighted.js
+++ test/weighted.js
@@ -2,15 +2,23 @@ import { expect } from 'chai';
 import { weighted } from '../src';
 
 describe('weighted pseudorandom number generator', () => {
-  // TODO: Rename
-  it('should have tests', () => {
-    expect(weighted({
-      distribution: {
-        x: 4,
-        y: 1,
-        z: 2,
-      },
-      seed: 'hello.',
-    })).to.equal('y');
+  const distribution = {
+    x: 4,
+    y: 1,
+    z: 2,
+  };
+
+  describe('seeded', () => {
+    it('should have stochastic output', () => {
+      expect(weighted({ distribution })).to.be.a('string');
+    });
+  });
+
+  describe('unseeded', () => {
+    const seed = 'hello.';
+
+    it('should have deterministic output', () => {
+      expect(weighted({ distribution, seed })).to.equal('y');
+    });
   });
 });
