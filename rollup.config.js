4e113ef74a0fe65013ab195f376a0ff6274599b9
Andreas Stassivik <andreas@stassi.net>
2019-02-04 00:03:44 -0800

    map build outputs

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,28 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-const withInputAndPlugins = x => Object.assign(
-  x,
+const rollupConfig = [
   {
-    input: 'src/index.js',
-    plugins: [
-      builtins(),
-      resolve(),
-      commonjs(),
-      babel({ exclude: 'node_modules/**' }),
-      terser(),
-    ],
+    file: 'lib/implausible.esm.js',
+    format: 'esm',
+    sourcemap: true,
   },
-);
+  {
+    file: 'lib/implausible.umd.js',
+    format: 'umd',
+    name: 'implausible',
+    sourcemap: true,
+  },
+].map(output => ({
+  output,
+  input: 'src/index.js',
+  plugins: [
+    builtins(),
+    resolve(),
+    commonjs(),
+    babel({exclude: 'node_modules/**'}),
+    terser(),
+  ],
+}));
 
-export default [
-  withInputAndPlugins({
-    output: {
-      file: 'lib/implausible.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-  }),
-  withInputAndPlugins({
-    output: {
-      file: 'lib/implausible.umd.js',
-      format: 'umd',
-      name: 'implausible',
-      sourcemap: true,
-    },
-  }),
-];
+export default rollupConfig;
