5cd6b7c0120c02de49355d3cfd678c70a5cd4757
Andreas Stassivik <andreas@stassi.net>
2019-02-05 06:26:17 -0800

    semantic functional composition

--- src/prng.js
+++ src/prng.js
@@ -1,14 +1,58 @@
+import {
+  ifElse,
+  isNil,
+  pipe,
+  prop,
+  propEq,
+  propSatisfies,
+  when,
+} from 'ramda';
 import seedrandom from 'seedrandom';
 
-const arc4 = seed => seedrandom(seed);
+const setObjectAsDefaultParameter = (param = {}) => ({ ...param });
 
-const setDefaultsAndApplySeed = ({
-  seed,
-  name = 'arc4',
-}) => name === 'arc4'
-  ? arc4(seed)
-  : seedrandom[name](seed || arc4());
+const setDefaultName = ({ name = 'arc4', ...props }) => ({ ...props, name });
 
-const prng = (params = {}) => setDefaultsAndApplySeed(params)();
+const propNameEqualsArc4 = propEq(
+  'name',
+  'arc4',
+);
+
+const prngFromSeedProp = pipe(
+  prop('seed'),
+  seedrandom,
+);
+
+const whenSeedPropIsNil = when(
+  propSatisfies(
+    isNil,
+    'seed',
+  ),
+);
+
+const generateSeedWhenPropIsNil = whenSeedPropIsNil(({ ...props }) =>
+  ({ ...props, seed: seedrandom() }));
+
+const prngFromNameAndSeedProps = ({ name, seed }) => seedrandom[name](seed);
+
+const ensureSeededPrng = pipe(
+  generateSeedWhenPropIsNil,
+  prngFromNameAndSeedProps,
+);
+
+const selectPrng = ifElse(
+  propNameEqualsArc4,
+  prngFromSeedProp,
+  ensureSeededPrng,
+);
+
+const callPrng = x => x();
+
+const prng = pipe(
+  setObjectAsDefaultParameter,
+  setDefaultName,
+  selectPrng,
+  callPrng,
+);
 
 export default prng;
