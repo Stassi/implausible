aa0df4a5a3ec45345c5267ee5f068f12d215db5f
Andreas Stassivik <andreas@stassi.net>
2019-02-06 14:10:08 -0800

    formatting

--- src/sample/uniformToWeightedWhenDetected.js
+++ src/sample/uniformToWeightedWhenDetected.js
@@ -11,10 +11,7 @@ const propIsArray = propIs(Array);
 const collectionPropIsArray = propIsArray('collection');
 
 const repeatOne = repeat(1);
-const repeatOneUntilLengthOf = pipe(
-  length,
-  repeatOne,
-);
+const repeatOneUntilLengthOf = pipe(length, repeatOne);
 
 const distributeUniformWeight = collection => zipObj(
   collection,
