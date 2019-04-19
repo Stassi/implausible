0e031b966d6fd48d877f6dee8e79d31765e1674c
Andreas Stassivik <andreas@stassi.net>
2019-04-19 12:28:03 -0700

    formatting

--- src/samples/uniformToWeightedWhenDetected.js
+++ src/samples/uniformToWeightedWhenDetected.js
@@ -9,7 +9,6 @@ const propIsArray = propIs(Array);
 const collectionPropIsArray = propIsArray('collection');
 
 const histogram = countBy(identity);
-
 const toHistogram = ({ collection, ...props }) => ({
   ...props,
   collection: histogram(collection),
