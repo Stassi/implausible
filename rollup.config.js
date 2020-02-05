538257b69cf4b4447310a80ddc9116a7edea784a
Andreas Stassivik <andreas@stassi.net>
2020-02-05 01:47:57 -0800

    rollup-plugin-node-polyfills replaces rollup-plugin-node-builtins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
-import builtins from 'rollup-plugin-node-builtins'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 import { terser } from 'rollup-plugin-terser'
 
 const rollupConfig = [
@@ -20,7 +20,7 @@ const rollupConfig = [
   output,
   input: 'src/index.js',
   plugins: [
-    builtins(),
+    nodePolyfills(),
     resolve(),
     commonjs(),
     babel({ exclude: 'node_modules/**' }),
