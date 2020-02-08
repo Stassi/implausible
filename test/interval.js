aa2b9c2a300763215262e280744de02e002178af
Andreas Stassivik <andreas@stassi.net>
2020-02-08 10:41:10 -0800

    test (wip)

--- test/interval.js
+++ test/interval.js
@@ -1,5 +1,10 @@
 import { describe, it } from 'mocha'
+import { expect } from 'chai'
+
+const interval = () => 0
 
 describe('#interval', () => {
-  it('needs tests')
+  it('needs tests', () => {
+    expect(interval()).to.equal(1)
+  })
 })
