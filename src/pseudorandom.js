f0f24178a61ea8a918caab66428bae9512b773bf
Andreas Stassivik <andreas@stassi.net>
2020-02-17 13:16:22 -0800

    name-variant compound methods

--- src/pseudorandom.js
+++ src/pseudorandom.js
@@ -4,14 +4,20 @@ import {
   map,
   strictlyEquals
 } from 'neida'
-import fromEntries from './utilities/fromEntries'
-import prngNames from './prngNames'
+import entries from '../src/utilities/entries'
+import flatten from '../src/utilities/flatten'
+import fromEntries from '../src/utilities/fromEntries'
+import prngNames from '../src/prngNames'
 
-const prng = ({ name, seed }) => {
+const prng = ({
+  prngName,
+  seed,
+  variantName
+}) => {
   const generic = conditional({
-    ifFalse: () => new seedrandom[name](seed),
+    ifFalse: () => new seedrandom[prngName](seed),
     ifTrue: () => seedrandom(seed),
-    predicate: () => strictlyEquals(name, 'arc4')
+    predicate: () => strictlyEquals(prngName, 'arc4')
   })
 
   const {
@@ -25,20 +31,31 @@ const prng = ({ name, seed }) => {
     generic,
     int32,
     quick
-  }
+  }[variantName]
 }
 
-const pseudorandom = fromEntries(
-  map({
-    data: prngNames,
-    transform: name => ([
-      name,
-      seed => prng({
-        name,
-        seed
+const pseudorandom = seed => fromEntries(
+  flatten(
+    map({
+      data: prngNames,
+      transform: prngName => map({
+        data: entries({
+          double: `${prngName}Double`,
+          generic: prngName,
+          int32: `${prngName}Int32`,
+          quick: `${prngName}Quick`
+        }),
+        transform: ([variantName, compoundName]) => [
+          compoundName,
+          prng({
+            prngName,
+            seed,
+            variantName
+          })
+        ]
       })
-    ])
-  })
+    })
+  )
 )
 
 export default pseudorandom
--- test/pseudorandom.js
+++ test/pseudorandom.js
@@ -2,66 +2,58 @@ import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import entries from '../src/utilities/entries'
 import forEach from '../src/utilities/forEach'
-import properties from '../src/utilities/properties'
 import pseudorandom from '../src/pseudorandom'
 import prngValues from './prngValues'
 
 describe('#pseudorandom', () => {
   const { algorithms, seed } = prngValues
 
-  forEach({
-    callback: ([, prngName]) => {
-      describe(`prng: ${prngName}`, () => {
-        describe('without seed', () => {
-          const prng = pseudorandom[prngName]()
+  describe('with seed', () => {
+    const prngs = pseudorandom(seed)
 
-          describe('variant: int32', () => {
-            it('should generate a non-deterministic number', () => {
-              expect(prng.int32()).to.be.a('number')
-            })
-          })
+    forEach({
+      callback: ([, [prngName, prngValues]]) => {
+        describe(`prng: ${prngName}`, () => {
+          const prng = prngs[prngName]
 
           forEach({
-            callback: ([, name]) => {
-              describe(`variant: ${name}`, () => {
-                it('should generate a non-deterministic interval', () => {
-                  expect(prng[name]()).to.be.at.least(0).and.below(1)
-                })
-              })
-            },
-            data: [
-              'double',
-              'generic',
-              'quick'
-            ]
-          })
-        })
-
-        describe('with seed', () => {
-          const prng = pseudorandom[prngName](seed)
-
-          forEach({
-            callback: ([, [generation, values]]) => {
+            callback: ([generation, prngValue]) => {
               describe(`generation: ${generation}`, () => {
-                forEach({
-                  callback: ([name, value]) => {
-                    describe(`variant: ${name}`, () => {
-                      it('should generate a deterministic number', () => {
-                        expect(prng[name]()).to.equal(value)
-                      })
-                    })
-                  },
-                  data: values
+                it('should generate a deterministic number', () => {
+                  expect(prng()).to.equal(prngValue)
                 })
               })
             },
-            data: entries(
-              algorithms[prngName]
-            )
+            data: prngValues
           })
         })
-      })
-    },
-    data: properties(pseudorandom)
+      },
+      data: entries(algorithms)
+    })
+  })
+
+  describe('without seed', () => {
+    const prngs = pseudorandom()
+
+    forEach({
+      callback: ([, [prngName, prngValues]]) => {
+        describe(`prng: ${prngName}`, () => {
+          const prng = prngs[prngName]
+
+          forEach({
+            callback: ([generation]) => {
+              describe(`generation: ${generation}`, () => {
+                it('should generate a non-deterministic number', () => {
+                  expect(prng()).to.be.a('number')
+                })
+              })
+            },
+            // TODO:
+            data: prngValues
+          })
+        })
+      },
+      data: entries(algorithms)
+    })
   })
 })
