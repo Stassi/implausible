700a16fff7932d62c41eff566fd9b01aa7bd64cf
Andreas Stassivik <andreas@stassi.net>
2020-02-25 20:53:23 -0800

    replenish depleted unitWeightPairs

--- src/sample.js
+++ src/sample.js
@@ -31,78 +31,88 @@ const sample = ({
   collection: collectionInput,
   intervals: intervalsInput,
   ...intervalOptions
-}) => until({
-  initialValue: {
-    intervals: conditional({
-      ifFalse: () => interval(intervalOptions),
-      ifTrue: () => intervalsInput,
-      predicate: () => intervalsInput
-    }),
-    results: [],
-    unitWeightPairs: map({
-      data: collectionInput,
-      transform: x => conditional({
-        ifFalse: () => [x, 1],
-        ifTrue: () => x,
-        predicate: () => isArray(x)
-      })
+}) => {
+  const initialUnitWeightPairs = map({
+    data: collectionInput,
+    transform: x => conditional({
+      ifFalse: () => [x, 1],
+      ifTrue: () => x,
+      predicate: () => isArray(x)
     })
-  },
-  predicate: ({ intervals }) => zeroLength(intervals),
-  transform: ({
-    intervals,
-    results,
-    unitWeightPairs
-  }) => {
-    const units = mapHead(unitWeightPairs)
+  })
 
-    const chosenUnitIndex = findIndex({
-      collection: zip(
-        units,
-        trimWeights(
-          reduce({
-            data: map({
-              data: unitWeightPairs,
-              transform: ([, weight]) => divide({
-                dividend: weight,
-                divisor: sum(
-                  ...mapTail(unitWeightPairs)
+  return until({
+    initialValue: {
+      intervals: conditional({
+        ifFalse: () => interval(intervalOptions),
+        ifTrue: () => intervalsInput,
+        predicate: () => intervalsInput
+      }),
+      results: [],
+      unitWeightPairs: initialUnitWeightPairs
+    },
+    predicate: ({ intervals }) => zeroLength(intervals),
+    transform: ({
+      intervals,
+      results,
+      unitWeightPairs: depletedUnitWeightPairs
+    }) => {
+      const unitWeightPairs = conditional({
+        ifFalse: () => depletedUnitWeightPairs,
+        ifTrue: () => initialUnitWeightPairs,
+        predicate: () => zeroLength(depletedUnitWeightPairs)
+      })
+
+      const units = mapHead(unitWeightPairs)
+
+      const chosenUnitIndex = findIndex({
+        collection: zip(
+          units,
+          trimWeights(
+            reduce({
+              data: map({
+                data: unitWeightPairs,
+                transform: ([, weight]) => divide({
+                  dividend: weight,
+                  divisor: sum(
+                    ...mapTail(unitWeightPairs)
+                  )
+                })
+              }),
+              initialValue: [0],
+              reducer: (sumOfPreviousWeights, weight) => [
+                ...sumOfPreviousWeights,
+                add(
+                  weight,
+                  tail(sumOfPreviousWeights)
                 )
-              })
-            }),
-            initialValue: [0],
-            reducer: (sumOfPreviousWeights, weight) => [
-              ...sumOfPreviousWeights,
-              add(
-                weight,
-                tail(sumOfPreviousWeights)
-              )
-            ]
-          })
-        )
-      ),
-      predicate: ([, weight]) => lessThan({
-        highest: weight,
-        value: head(intervals)
+              ]
+            })
+          )
+        ),
+        predicate: ([, weight]) => lessThan({
+          highest: weight,
+          value: head(intervals)
+        })
       })
-    })
 
-    return {
-      intervals: withoutHead(intervals),
-      results: [
-        ...results,
-        units[chosenUnitIndex]
-      ],
-      unitWeightPairs: conditional({
-        ifFalse: () => removeIndex({
-          collection: unitWeightPairs,
-          index: chosenUnitIndex
-        }),
-        ifTrue: () => unitWeightPairs,
-        predicate: () => replacement
-      })
+      return {
+        intervals: withoutHead(intervals),
+        results: [
+          ...results,
+          units[chosenUnitIndex]
+        ],
+        unitWeightPairs: conditional({
+          ifFalse: () => removeIndex({
+            collection: unitWeightPairs,
+            index: chosenUnitIndex
+          }),
+          ifTrue: () => unitWeightPairs,
+          predicate: () => replacement
+        })
+      }
     }
-  }
-}).results
+  }).results
+}
 
 export default sample
