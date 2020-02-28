31963b1199053a6559abeac55e6909e74167232d
Andreas Stassivik <andreas@stassi.net>
2020-02-27 23:16:38 -0800

    implement shuffle

---
+++ src/shuffle.js
@@ -0,0 +1,30 @@
+import { cardinality as length } from 'neida'
+import chunk from './utilities/chunk'
+import multiply from './utilities/multiply'
+import sample from './sample'
+
+const shuffle = ({
+  collection,
+  count,
+  prng,
+  replacement,
+  seed
+}) => {
+  const collectionLength = length(collection)
+
+  return chunk({
+    collection: sample({
+      collection,
+      prng,
+      replacement,
+      seed,
+      count: multiply(
+        collectionLength,
+        count
+      )
+    }),
+    length: collectionLength
+  })
+}
+
+export default shuffle
